7d5a43e6e4df340f56c2a2d2fbeb4ff87173c2d7
nrhint <nathan@hintonclan.org>
2021-09-27 14:56:08 -0700

    Working at the basics

--- chicken.py
+++ chicken.py
@@ -1,47 +1,60 @@
 ##Nathan Hinton
 ##This file will have all of the parts for the chickens
 
+import pygame
 from random import randint
+from bar import Bar
 
-vv = True
+vv = False
 
 class Chicken:
-    def __init__(self, image, x = 0, y = 0, v = False):
-        self.food = 50 #Range of 0-100
-        self.hydration = 50 #Range of 0-100
+    def __init__(self, screen, image, x = 0, y = 0, v = False):
         self.image = image
         self.rect = self.image.get_rect()
-        self.rect.move(x, y)
+        self.rect = self.rect.move(x, y)
+        self.screen = screen
         self.x = x
         self.y = y
+        self.lastx = self.x
+        self.lasty = self.y
+        self.food = 50 #Range of 0-100
+        self.foodBar = Bar(self.x, self.y, 100, 10, self.screen, color = (150, 150, 0))
+        self.hydration = 50 #Range of 0-100
+        self.hydrationBar = Bar(self.x, self.y+10, 100, 10, self.screen, color = (100, 100, 200))
+        self.foodMax = 100
+        self.hydrationMax = 100
+        
         self.v = v #verbose output
+        if self.v: print(self.rect)
 
     def eat(self, foodItem = None):
         if foodItem is not None:
             if self.v: print("Giving %s to chicken %s"%(foodItem, self))
             if foodItem == 'basic':
-                self.food += 1
+                self.food += 10
             else:
                 print("Invalid food (%s) given to chicken (%s)."%(self, foodItem))
+        if self.food > self.foodMax: self.food = self.foodMax
         if self.v: self.status()
             
     def drink(self, drinkItem = None):
         if drinkItem is not None:
             if self.v: print("Giving %s to chicken %s"%(drinkItem, self))
             if drinkItem == 'water':
-                self.hydration += 1
+                self.hydration += 10
             else:
                 print("Invalid hydrate (%s) given to chicken (%s)."%(drinkItem, self))
+        if self.hydration > self.hydrationMax: self.hydration = self.hydrationMax
         if self.v: self.status()
 
-    def hunger(self, chance = 3, amount = 2):
-        if randint(0, 100) < chance:
+    def hunger(self, chance = 20, amount = 1):
+        if randint(0, 1000) < chance:
             self.food -= amount
             if vv:print("%s got hungry"%self)
         if self.v: self.status()
 
-    def thirst(self, chance = 3, amount = 2):
-        if randint(0, 100) < chance:
+    def thirst(self, chance = 20, amount = 1):
+        if randint(0, 1000) < chance:
             self.hydration -= amount
             if vv:print("%s got thirsty"%self)
         if self.v: self.status()
@@ -56,21 +69,32 @@ class Chicken:
         self.hunger()
         self.thirst()
         if self.food <= 0:
-            if self.v or vv: print("Chicken %s died from hunger..."%self)
+            if self.v or vv or True: print("Chicken %s died from hunger..."%self)
             if self.v or vv: self.status()
             return True
         if self.hydration <= 0:
-            if self.v or vv: print("Chicken %s died from thirst..."%self)
+            if self.v or vv or True: print("Chicken %s died from thirst..."%self)
             if self.v or vv: self.status()
             return True
         #if self.v: self.status()
+        self.rect = self.rect.move(self.x-self.lastx, self.y-self.lasty)
+        self.lastx = self.x
+        self.lasty = self.y
+        self.foodBar.update(value = self.food)
+        self.hydrationBar.update(value = self.hydration)
+        if vv: pygame.draw.rect(self.screen, (200, 0, 0), self.rect, 1)
         return False
+
+    def draw(self):
+        self.foodBar.draw()
+        self.hydrationBar.draw()
+        self.screen.blit(self.image, (self.x, self.y))
 
     def input(self, mouseState): ##This will check if the chicken is clicked on.
         if self.v: print(mouseState)
-        if mouseState == 1: #The left button is clicked:
+        if mouseState == 4: #The left button is clicked:
             self.eat('basic')
-        elif mouseState == 3: #The right button is clicked:
+        elif mouseState == 5: #The right button is clicked:
             self.drink('water')
         else:
             print("Invalid click value on chicken %s"%self)
--- main.py
+++ main.py
@@ -2,20 +2,22 @@
 ##This file will contain the code for running the window.
 
 import pygame
+from random import randint
+
 from chicken import Chicken
+from text import Text
 
 v = True
 vv = False
 
 pygame.init()
+font = pygame.font.Font(None, 36)
+
 
 width=350
 height=400
 screen = pygame.display.set_mode( (width, height ) )
 pygame.display.set_caption('feed my chicken(s)')
-
-FPS = 30
-fpsClock = pygame.time.Clock()
 
 #colors:
 BACKGROUND_GREEN = (65, 132, 18)
@@ -25,13 +27,20 @@ RED = (255, 0, 0)
 GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 
+#FPS stuff
+FPS = 30
+fpsClock = pygame.time.Clock()
+fpsText = Text(screen, font, "loading...", BLACK, (50, 10))
+
 ##Load the images. This may be moved to another file later on
 chickenImage = pygame.image.load("images/chicken.png")
 
 ##Setup the objects lists:
 chickens = []
 for x in range(0, 1):
-    chickens.append(Chicken(chickenImage, x = 50, y = 50, v = vv))
+    chickens.append(Chicken(screen, chickenImage, x = 50, y = 50, v = vv))
+
+chickenCount = Text(screen, font, "loading...", BLACK, (200, 10))
 
 screen.fill(BACKGROUND_GREEN)
 pygame.display.flip()
@@ -46,15 +55,23 @@ while (running):
             x, y = event.pos
             for chicken in chickens:
                 if chicken.rect.collidepoint(x, y):
-                    if v: print('clicked on chicken')
+                    if vv: print('clicked on chicken')
                     chicken.input(event.button)
+    if randint(0, 1000) < 1:
+        chickens.append(Chicken(screen, chickenImage, x = randint(50, 200), y = randint(50, 250), v = vv))
     ##Update screen:
     screen.fill(BACKGROUND_GREEN)
     for chicken in chickens:
         died = chicken.update()
         if died:
             chickens.remove(chicken)
-        screen.blit(chickenImage, (chicken.x, chicken.y))
+            #print("Chicken %s died"%chicken)
+        chicken.draw()
+        #screen.blit(chickenImage, (chicken.x, chicken.y))
+    fpsText.draw()
+    fpsText.update(f"{fpsClock.get_fps():2.0f} FPS")
+    chickenCount.draw()
+    chickenCount.update("%s chicken(s)"%len(chickens))
     pygame.display.flip()
     fpsClock.tick(FPS)
 
