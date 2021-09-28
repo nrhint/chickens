5469f5ab7450adbcf9112a85addbf39f44f75e26
nrhint <nathan@hintonclan.org>
2021-09-27 18:25:34 -0700

    added eggs into the game

--- chicken.py
+++ chicken.py
@@ -7,6 +7,10 @@ from bar import Bar
 
 vv = False
 
+EGG_LAIED = pygame.USEREVENT+1
+eggLaied = pygame.event.Event(EGG_LAIED, message="Egg!")
+
+
 class Chicken:
     def __init__(self, screen, image, x = 0, y = 0, v = False):
         self.image = image
@@ -18,11 +22,14 @@ class Chicken:
         self.lastx = self.x
         self.lasty = self.y
         self.food = 50 #Range of 0-100
-        self.foodBar = Bar(self.x, self.y, 100, 10, self.screen, color = (150, 150, 0))
+        self.foodBar = Bar(self.x, self.y, 100, 8, self.screen, color = (150, 150, 0))
         self.hydration = 50 #Range of 0-100
-        self.hydrationBar = Bar(self.x, self.y+10, 100, 10, self.screen, color = (100, 100, 200))
+        self.hydrationBar = Bar(self.x, self.y+10, 100, 8, self.screen, color = (100, 100, 200))
         self.foodMax = 100
         self.hydrationMax = 100
+        self.moving = False
+        self.speed = 1
+        self.targetPos = (self.x, self.y)
         
         self.v = v #verbose output
         if self.v: print(self.rect)
@@ -31,7 +38,7 @@ class Chicken:
         if foodItem is not None:
             if self.v: print("Giving %s to chicken %s"%(foodItem, self))
             if foodItem == 'basic':
-                self.food += 10
+                self.food += 1
             else:
                 print("Invalid food (%s) given to chicken (%s)."%(self, foodItem))
         if self.food > self.foodMax: self.food = self.foodMax
@@ -41,19 +48,19 @@ class Chicken:
         if drinkItem is not None:
             if self.v: print("Giving %s to chicken %s"%(drinkItem, self))
             if drinkItem == 'water':
-                self.hydration += 10
+                self.hydration += 1
             else:
                 print("Invalid hydrate (%s) given to chicken (%s)."%(drinkItem, self))
         if self.hydration > self.hydrationMax: self.hydration = self.hydrationMax
         if self.v: self.status()
 
-    def hunger(self, chance = 20, amount = 1):
+    def hunger(self, chance = 4, amount = 1):
         if randint(0, 1000) < chance:
             self.food -= amount
             if vv:print("%s got hungry"%self)
         if self.v: self.status()
 
-    def thirst(self, chance = 20, amount = 1):
+    def thirst(self, chance = 4, amount = 1):
         if randint(0, 1000) < chance:
             self.hydration -= amount
             if vv:print("%s got thirsty"%self)
@@ -65,9 +72,30 @@ class Chicken:
         print("Food level: %s"%self.food)
         print("Hydration level: %s"%self.hydration)
 
+    def move(self):
+        if not self.moving:
+            if randint(0, 1000) < (self.food+self.hydration)/16:
+                self.moving = True
+                self.targetPos = (randint(50, 350), randint(50, 500))
+                #print("Moving to (%s, %s)"%self.targetPos)
+        else:
+            if self.x > self.targetPos[0]:
+                self.x -= self.speed
+            elif self.x < self.targetPos[0]:
+                self.x += self.speed
+            if self.y > self.targetPos[1]:
+                self.y -= self.speed
+            elif self.y < self.targetPos[1]:
+                self.y += self.speed
+            if (self.x, self.y) == self.targetPos:
+                self.moving = False
+                pygame.event.post(eggLaied)
+                #print("Finished moving")
+
     def update(self):
         self.hunger()
         self.thirst()
+        self.move()
         if self.food <= 0:
             if self.v or vv or True: print("Chicken %s died from hunger..."%self)
             if self.v or vv: self.status()
@@ -80,8 +108,8 @@ class Chicken:
         self.rect = self.rect.move(self.x-self.lastx, self.y-self.lasty)
         self.lastx = self.x
         self.lasty = self.y
-        self.foodBar.update(value = self.food)
-        self.hydrationBar.update(value = self.hydration)
+        self.foodBar.update(x = self.x, y = self.y, value = self.food)
+        self.hydrationBar.update(x = self.x, y = self.y+8, value = self.hydration)
         if vv: pygame.draw.rect(self.screen, (200, 0, 0), self.rect, 1)
         return False
 
@@ -90,7 +118,7 @@ class Chicken:
         self.hydrationBar.draw()
         self.screen.blit(self.image, (self.x, self.y))
 
-    def input(self, mouseState): ##This will check if the chicken is clicked on.
+    def input(self, mouseState, other = False): ##This will check if the chicken is clicked on.
         if self.v: print(mouseState)
         if mouseState == 4: #The left button is clicked:
             self.eat('basic')
--- main.py
+++ main.py
@@ -4,7 +4,7 @@
 import pygame
 from random import randint
 
-from chicken import Chicken
+from chicken import *
 from text import Text
 
 v = True
@@ -14,8 +14,8 @@ pygame.init()
 font = pygame.font.Font(None, 36)
 
 
-width=350
-height=400
+width=450
+height=600
 screen = pygame.display.set_mode( (width, height ) )
 pygame.display.set_caption('feed my chicken(s)')
 
@@ -36,15 +36,18 @@ fpsText = Text(screen, font, "loading...", BLACK, (50, 10))
 chickenImage = pygame.image.load("images/chicken.png")
 
 ##Setup the objects lists:
+eggs = 0
 chickens = []
 for x in range(0, 1):
     chickens.append(Chicken(screen, chickenImage, x = 50, y = 50, v = vv))
 
 chickenCount = Text(screen, font, "loading...", BLACK, (200, 10))
+eggCount = Text(screen, font, "loading...", BLACK, (50, 540))
 
 screen.fill(BACKGROUND_GREEN)
 pygame.display.flip()
 
+frameCount = 0
 running = True
 while (running):
     for event in pygame.event.get():
@@ -57,8 +60,14 @@ while (running):
                 if chicken.rect.collidepoint(x, y):
                     if vv: print('clicked on chicken')
                     chicken.input(event.button)
-    if randint(0, 1000) < 1:
-        chickens.append(Chicken(screen, chickenImage, x = randint(50, 200), y = randint(50, 250), v = vv))
+        if event.type == EGG_LAIED:
+            eggs += 1
+    #Spawn chickens
+    if randint(0, 10000) < 3 and len(chickens) < 100:
+        chickens.append(Chicken(screen, chickenImage, x = randint(50, 350), y = randint(50, 500), v = vv))
+    #add eggs when needed:
+    if frameCount % 1800 == 0:
+        eggs += len(chickens)
     ##Update screen:
     screen.fill(BACKGROUND_GREEN)
     for chicken in chickens:
@@ -72,6 +81,9 @@ while (running):
     fpsText.update(f"{fpsClock.get_fps():2.0f} FPS")
     chickenCount.draw()
     chickenCount.update("%s chicken(s)"%len(chickens))
+    eggCount.draw()
+    eggCount.update("%s eggs"%eggs)
+    frameCount += 1
     pygame.display.flip()
     fpsClock.tick(FPS)
 
